4c0f8934a7a13fecd28dcca0fe0d3c1f00165a6b
turunco <yumi.kataoka.2015@gmail.com>
2017-02-24 01:32:18 -0800

    add basic test

---
+++ js/main.js
@@ -0,0 +1,14 @@
+class Main {
+    // public
+
+    constructor() {}
+
+    // private
+
+    makeMessage_(message) {
+        return 'Hello, ' + message;
+    }
+}
+
+
+module.exports.Main = Main;
---
+++ test/test-main.js
@@ -0,0 +1,50 @@
+// called once
+before(() => {
+    console.log('before');
+});
+after(() => {
+    console.log('before');
+});
+
+
+// called each it
+beforeEach(() => {
+    console.log('beforeEach');
+});
+afterEach(() => {
+    console.log('afterEach');
+});
+
+
+//
+// SA) https://mochajs.org/#bdd
+//
+var assert = require('assert');
+var Main = require('../js/main.js').Main;
+describe('class Main', function() {
+    describe('makeMessage_', function() {
+
+        it('test 0', () => {
+            var main = new Main();
+            assert.equal('Hello, ', main.makeMessage_(''));
+        });
+
+        it('test 1', () => {
+            var main = new Main();
+            assert.equal('Hello, World!', main.makeMessage_('World!'));
+        });
+
+        // specify() is alias for it()
+        specify('test 2', () => {
+            var main = new Main();
+            assert.equal('Hello, 123', main.makeMessage_(123));
+        });
+    });
+});
+
+// context is alias for describe
+context('class Main test2', () => {
+    it('test 0', () => {
+        assert.equal(1, 1);
+    });
+});
